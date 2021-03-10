193d434ee85d2c6b91caaf2fefd7a225b95478a3
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2021-03-10 21:45:12 +0600

    donation newly added

---
+++ hunger-solver/Controllers/DonationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hunger_solver.Controllers
+{
+    public class DonationController : Controller
+    {
+        // GET: Donation
+        public ActionResult FoodDonation()
+        {
+            return View();
+        }
+
+        public ActionResult ClothDonation()
+        {
+            return View();
+        }
+
+        public ActionResult MoneyDonation()
+        {
+            return View();
+        }
+
+        public ActionResult BloodDonation()
+        {
+            return View();
+        }
+    }
+}
